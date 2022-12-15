46d2eb0604e78d1107e75fa366bc29eb68a54009
Henrique Moura <henriq8383@gmail.com>
2022-12-15 19:09:08 -0300

    início inteligencia artificial goblin

--- metroidvania/Assets/Scripts/Goblin.cs
+++ metroidvania/Assets/Scripts/Goblin.cs
@@ -6,11 +6,14 @@ public class Goblin : MonoBehaviour
 {
     private Rigidbody2D rig;
     private bool isFront;
+    private Vector2 direction = Vector2.right;
 
     public float speed;
     public float maxVision;
     public Transform point;
 
+    public bool isRight;
+    public float stopDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -21,29 +24,62 @@ public class Goblin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    void OnMove()
+    {
+        if (isFront)
+        {
+            if (isRight) //vira para a direita
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+                direction = Vector2.right;
+                rig.velocity = new Vector2(speed, rig.velocity.y);
+            }
+            else //vira para esquerda
+            {
+                transform.eulerAngles = new Vector2(0, 180);
+                direction = Vector2.left;
+                rig.velocity = new Vector2(-speed, rig.velocity.y);
+            }
+        }
+        
+        
     }
 
     void FixedUpdate()
     {
         GetPlayer();
+
+        OnMove();
     }
 
     void GetPlayer()
     {
-        RaycastHit2D hit = Physics2D.Raycast(point.position, Vector2.right, maxVision);
+        RaycastHit2D hit = Physics2D.Raycast(point.position, direction, maxVision);
 
         if (hit.collider != null)
         {
             if (hit.transform.CompareTag("Player"))
             {
-                Debug.Log("Vendo o player.");
+                isFront = true;
+
+                float distance = Vector2.Distance(transform.position, hit.transform.position);
+
+                if (distance <= stopDistance)
+                {
+                    isFront = false;
+
+                }
             }
         }
+
+        
     }
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.DrawRay(point.position, Vector2.right * maxVision);
+        Gizmos.DrawRay(point.position, direction * maxVision);
     }
 }
